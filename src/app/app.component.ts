7852ec09fc6ee570b97f2f13bd7230da8b7d83c6
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2024-07-07 17:48:28 -0500

    migrated app from standalone to module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
+import { Component } from '@angular/core';
+// import { HeaderComponent } from './header/header.component';
+// import { UserComponent } from './user/user.component';
+// import { TasksComponent } from './tasks/tasks.component';
 
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  // standalone: true,
+  // imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
